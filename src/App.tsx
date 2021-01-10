4fb1487634c194f3837db939e3e3b6dedae88a1d
Lari Löfman <larwazor@gmail.com>
2021-01-10 16:45:54 +0200

    add room generator

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Player from './components/player';
 import Npc from './components/npc';
 import Map from './components/map';
+import { useStateValue } from './components/state';
 
 const App = () => {
+  const [{ mapData, tileSize }] = useStateValue();
 
   return (
-    <div className="zone-container" >
+    <div
+      className="zone-container"
+      style={{
+        width: mapData.size.w * tileSize.w,
+        height: mapData.size.h * tileSize.h
+      }}>
       <Player skin="f1" />
       <Npc skin="e1" />
       <Map />
--- src/hooks/use-generate-map/index.ts
+++ src/hooks/use-generate-map/index.ts
@@ -1,7 +1,71 @@
-import { Direction, Dimensions, MapData, TileType, Tile } from '../../types';
-import randomEnum from '../../utils/random-enum';
+import { Dimensions, MapData, TileType, Tile, Position } from '../../types';
 
 export default function useGenerateMap() {
+
+    const generateRoom = (size: Dimensions, mapSize: Dimensions, pos: Position, tiles: Tile[][]) => {
+        // const tiles = [];
+        // for (let y = pos.y; y < pos.y + size.w; y++) {
+        //     const row: Tile[] = [];
+        //     for (let x = pos.x; x < pos.x + size.h; x++) {
+        //         if (y === pos.y || x === pos.x) {
+
+        //             const tile = {
+        //                 type: TileType.wall,
+        //                 id: (size.h * y) + x,
+        //                 position: { x, y },
+        //                 passable: false
+        //             };
+
+        //             row.push(tile);
+        //         }
+
+        //     }
+        //     tiles.push(row);
+        // }
+
+        const nonCornerTiles = [];
+
+        for (let y = pos.y; y < pos.y + size.w; y++) {
+            for (let x = pos.x; x < pos.x + size.h; x++) {
+                if (y === pos.y ||
+                    x === pos.x ||
+                    y === pos.y + size.h - 1 ||
+                    x === pos.x + size.w - 1) {
+
+                    const tile = {
+                        type: TileType.wall,
+                        id: (size.h * y) + x,
+                        position: { x, y },
+                        passable: false
+                    };
+
+                    tiles[y][x] = tile;
+
+                    if (!(y === pos.y && x === pos.x) &&
+                        !(y === pos.y && x === pos.x + size.w - 1) &&
+                        !(x === pos.x && y === pos.y + size.h - 1) &&
+                        !(y === pos.y + size.h - 1 && x === pos.x + size.w - 1)
+                    ) {
+                        const nonCornerTile = {
+                            position: { x, y }
+                        };
+                        nonCornerTiles.push(nonCornerTile);
+                    }
+                }
+
+            }
+        }
+
+        const doorTile = nonCornerTiles[Math.floor(Math.random() * nonCornerTiles.length)];
+        tiles[doorTile.position.y][doorTile.position.x] = {
+            ...tiles[doorTile.position.y][doorTile.position.x],
+            type: TileType.floor,
+            passable: true
+        };
+        console.log(nonCornerTiles);
+
+        return tiles;
+    };
 
     const generateMap = (size: Dimensions): MapData => {
         const tiles = [];
@@ -22,9 +86,10 @@ export default function useGenerateMap() {
             }
             tiles.push(row);
         }
+        const tiles2 = generateRoom({ w: 5, h: 5 }, size, { x: 1, y: 1 }, tiles);
         return (
             {
-                tiles,
+                tiles: tiles2,
                 size
             }
         );
