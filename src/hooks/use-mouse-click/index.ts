c62a9895f9b69e2e50c054fbd51700a152f88079
Lari Löfman <larwazor@gmail.com>
2021-01-14 18:34:40 +0200

    modify click hook by taking into account camera's position

--- src/hooks/use-mouse-click/index.ts
+++ src/hooks/use-mouse-click/index.ts
@@ -5,7 +5,7 @@ import { useStateValue } from '../../components/state';
 export default function useMouseClick() {
     const [tileClicked, setTileClicked] = useState<Position | undefined>(undefined);
     const [zone, setZone] = useState<HTMLElement | null>(document.getElementById('zone-container'));
-    const [{ tileSize }] = useStateValue();
+    const [{ tileSize, cameraPosition }] = useStateValue();
     useEffect(() => {
         if (!zone) {
             setZone(document.getElementById("zone-container"));
@@ -18,15 +18,14 @@ export default function useMouseClick() {
     });
 
     const onClick = (e: MouseEvent) => {
-        // TODO: Take into account the camera position to get the actual clicked tile, not the one relative to camera
         if (zone) {
             const borderLeft = getComputedStyle(zone, null).getPropertyValue('border-left-width').replace(/\D/g, "") as unknown as number;
             const borderTop = getComputedStyle(zone, null).getPropertyValue('border-top-width').replace(/\D/g, "") as unknown as number;
 
-            const clickX = Math.floor((e.clientX - zone.offsetLeft - borderLeft) / tileSize.w);
-            const clickY = Math.floor((e.clientY - zone.offsetTop - borderTop) / tileSize.h);
+            // Get click on viewport -> subtract display element's offset and the border -> divide by tileSize -> floor down -> add camera's position
+            const clickX = Math.floor(((e.clientX - zone.offsetLeft - borderLeft) / tileSize.w)) + cameraPosition.x;
+            const clickY = Math.floor(((e.clientY - zone.offsetTop - borderTop) / tileSize.h)) + cameraPosition.y;
             setTileClicked({ x: clickX, y: clickY });
-
         }
     };
 
